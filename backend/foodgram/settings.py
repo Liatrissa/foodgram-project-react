ad00be3a0faaa67c59eab2eef7f8cd4e2f0d5b4f
Anastasiya <anastasiyaalexxx@gmail.com>
2023-06-06 13:48:30 +0300

    final

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -152,7 +152,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DATA_ROOT = os.path.join(BASE_DIR, 'data')
+PATH_DATA = Path(BASE_DIR, 'data/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
