a628007e6e5836bd8ee6aad0271a1c156391504c
Anastasiya <anastasiyaalexxx@gmail.com>
2023-06-06 18:51:29 +0300

    final

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -63,11 +63,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'foodgram.urls'
 
-TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
