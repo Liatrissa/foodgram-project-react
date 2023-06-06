4e76bf467e00171d4ee117069252d93e669bb254
Anastasiya <anastasiyaalexxx@gmail.com>
2023-06-06 21:47:16 +0300

    final

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,7 @@
-from django_filters import CharFilter
-from django_filters.rest_framework import BooleanFilter, FilterSet, filters
+from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, Recipe
+from users.models import User
 
 
 class IngredientFilter(FilterSet):
@@ -18,14 +18,33 @@ class RecipeFilter(FilterSet):
     Набор фильтров, которые можно применить к запросам, чтобы фильтровать
     объекты модели Recipe
     """
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all()
+    tags = filters.AllValuesMultipleFilter(
+        field_name='tags__slug', )
+    author = filters.ModelChoiceFilter(
+        queryset=User.objects.all(),
     )
-    author = CharFilter(field_name='author')
-    is_favorited = BooleanFilter()
-    is_in_shopping_cart = BooleanFilter()
+    is_favorited = filters.BooleanFilter(method='filter_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_shopping_cart'
+    )
+
+    def filter_favorited(self, queryset, name, value):
+        """
+        Фильтр возвращает объекты рецептов, которые находятся в избранном
+        для данного пользователя.
+        """
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(favorite__user=self.request.user)
+        return queryset
+
+    def filter_shopping_cart(self, queryset, name, value):
+        """
+        Фильтр возвращает объекты рецептов, которые находятся в корзине
+        для данного пользователя.
+        """
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(shopping_list__user=self.request.user)
+        return queryset
 
     class Meta:
         model = Recipe
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -19,4 +19,5 @@ router.register('recipes', RecipeViewSet, basename='recipes')
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
 ]
