d5d91f5c20574a7db444bd4858c9bbd408a38d15
Anastasiya <anastasiyaalexxx@gmail.com>
2023-06-09 00:27:07 +0300

    final

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nxa!1%g%4k9*y4_d)y7z7sgpmk0cnlv-u^ap0jtf(5=v7%6bv$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -60,11 +60,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'foodgram.urls'
-
+TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
